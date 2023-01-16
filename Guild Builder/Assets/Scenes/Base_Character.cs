1504df899b94fc32e63833dea897b88f2d84b3ca
Travonte <tvtokyoj@hotmail.com>
2023-01-15 19:04:25 -0800

    Update
    Updates to be able to pull onto desktop

---
+++ Guild Builder/Assets/Scenes/Base_Character.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base_Character
+{
+    public string name;
+    public string jobName;
+    public int level = 1;
+    public double health, maxHealth;
+
+
+}
